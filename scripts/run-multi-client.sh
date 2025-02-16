e01edd52f441e018a290d9ec694d941b7a5501bc
wql2002 <wqlthu2020@gmail.com>
2025-02-16 17:44:54 -0600

    test(proj1): update utility for testcase

--- scripts/run-multi-client.sh
+++ scripts/run-multi-client.sh
@@ -2,9 +2,23 @@
 
 # Read the number of clients from the command line
 SERVER_ADDR=$1
-NUM_CLIENTS=$2
+TEST_CASE=$2
 
-# Run the client concurrently
-for i in $(seq 1 $NUM_CLIENTS); do
-  cat traces/trace-1k-$i | just p1::benchmark ${SERVER_ADDR} &
-done
+# for test case 1/2, run single thread with trace: test_case_{case_number}_single.trace
+if [ $TEST_CASE -eq 1 ] || [ $TEST_CASE -eq 2 ]; then
+	cat traces/test_case_${TEST_CASE}_single.trace | just p1::benchmark ${SERVER_ADDR} &
+fi
+
+# for test case 3/4, run 3 threads with trace: test_case_3_client{num}.trace
+if [ $TEST_CASE -eq 3 ] || [ $TEST_CASE -eq 4 ]; then
+	cat traces/test_case_${TEST_CASE}_client1.trace | just p1::benchmark ${SERVER_ADDR} &
+	cat traces/test_case_${TEST_CASE}_client2.trace | just p1::benchmark ${SERVER_ADDR} &
+	cat traces/test_case_${TEST_CASE}_client3.trace | just p1::benchmark ${SERVER_ADDR} &
+fi
+
+if [ $TEST_CASE -eq 5 ]; then
+	cat traces/test_case_${TEST_CASE}_client1.trace | just p1::benchmark ${SERVER_ADDR} &
+	cat traces/test_case_${TEST_CASE}_client2.trace | just p1::benchmark ${SERVER_ADDR} &
+	cat traces/test_case_${TEST_CASE}_client3.trace | just p1::benchmark ${SERVER_ADDR} &
+	cat traces/test_case_${TEST_CASE}_client4.trace | just p1::benchmark ${SERVER_ADDR} &
+fi
